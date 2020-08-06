a253e863322f2e21b320afad6aae9f98d7b85fd4
renfei147 <renfei8@live.cn>
2020-08-06 10:17:09 +0800

    Update UI

--- client/src/MainScene.js
+++ client/src/MainScene.js
@@ -1,6 +1,7 @@
 var MainScene = cc.Scene.extend({
     ctor: function () {
         this._super();
+
         var background = new cc.Sprite(res.MainSceneBG_png);
         background.x = size.width / 2;
         background.y = size.height / 2;
@@ -12,68 +13,46 @@ var MainScene = cc.Scene.extend({
         mainTitle.scale = 0.8;
         this.addChild(mainTitle);
 
-        scrollableLayer = new cc.Layer();
-        this.addChild(scrollableLayer);
+        //修改昵称框
+        var renameModalBox = new ModalBox(600, 350);
+        this.addChild(renameModalBox, 10);
 
-        //登录界面
-        var loginUI = new ccui.Widget();
-        scrollableLayer.addChild(loginUI);
-        var titleLabel = creator.createLabel(txt.mainScene.loginTitle, 40);
-        titleLabel.setPosition(size.width / 2, size.height / 2 + 100);
-        loginUI.addChild(titleLabel);
+        var renameModalBoxLabel = creator.createLabel("修改昵称", 30);
+        renameModalBoxLabel.setPosition(size.width / 2, size.height / 2 + 100)
+        renameModalBox.addChild(renameModalBoxLabel);
 
         var nameBox = creator.createEditBox(txt.mainScene.enterName, cc.size(500, 60));
         var lastName = storage.getItem("name");
         if (lastName) {
             nameBox.setString(lastName);
         }
-        nameBox.setPosition(size.width / 2, size.height / 2 + 20);
-        loginUI.addChild(nameBox);
-
-        function moveToMainUI() {
-            messageLabel.opacity = 0;
-            clearTimeout(showMessageTID);
-            messageLabel.stopAllActions();
-            loginUI.enabled = false;
-            mainUI.enabled = true;
-            playOnlineButton.enabled = !player.guest;
-            var moveAction = cc.moveTo(1, cc.p(-size.width, 0)).easing(cc.easeExponentialInOut());
-            scrollableLayer.runAction(moveAction);
-            updatePlayerLabel();
-        }
-
-        var loginButton = creator.createButton(txt.mainScene.login, cc.size(250, 60), function () {
+        nameBox.setPosition(size.width / 2, size.height / 2);
+        renameModalBox.addChild(nameBox);
+        
+        confirmButton = creator.createButton("确认", cc.size(150, 60), function () {
             var name = nameBox.getString();
             if (name.length == 0) {
                 showMessage(txt.mainScene.emptyName);
             } else if (name.length > 15) {
                 showMessage(txt.mainScene.nameTooLong);
             } else {
-                loginUI.enabled = false;
                 player.login(nameBox.getString(), function () {
                     storage.setItem("name", name);
-                    moveToMainUI();
+                    renameModalBox.hide();
                 }, function (error) {
                     loginUI.enabled = true;
                     showMessage(error);
                 });
             }
-        });
 
-        loginButton.setPosition(size.width / 2, size.height / 2 - 90);
-        loginUI.addChild(loginButton);
-
-        guestButton = creator.createButton(txt.mainScene.loginAsGuest, cc.size(250, 60), function () {
-            player.loginAsGuest();
-            moveToMainUI();
         });
-        guestButton.setPosition(size.width / 2, size.height / 2 - 190);
-        loginUI.addChild(guestButton);
+        confirmButton.setPosition(size.width / 2, size.height / 2 - 100);
+        renameModalBox.addChild(confirmButton);
 
         var messageLabel = creator.createLabel("", 30, cc.color(255, 20, 20));
-        messageLabel.setPosition(size.width / 2, size.height / 2 - 270);
+        messageLabel.setPosition(size.width / 2, size.height / 2 - 220);
         messageLabel.opacity = 0;
-        this.addChild(messageLabel);
+        renameModalBox.addChild(messageLabel);
 
         var showMessageTID;
         function showMessage(text) {
@@ -86,11 +65,7 @@ var MainScene = cc.Scene.extend({
             }, 2000);
         }
 
-        //主界面层
-        var mainUI = new ccui.Widget();
-        mainUI.x = size.width;
-        scrollableLayer.addChild(mainUI);
-
+        //主界面
         var playOnlineButton = new ccui.Button(res.PlayOnlineButton_png);
         playOnlineButton.ignoreContentAdaptWithSize(false);
         playOnlineButton.setContentSize(130, 130);
@@ -98,47 +73,30 @@ var MainScene = cc.Scene.extend({
         playOnlineButton.addClickEventListener(function () {
             cc.director.pushScene(new OnlineGameScene());
         });
-        mainUI.addChild(playOnlineButton);
+        this.addChild(playOnlineButton);
 
         var playWithAIButton = creator.createButton(txt.mainScene.playWithAI, cc.size(280, 60), function () {
             cc.director.pushScene(new AIGameScene());
         });
         playWithAIButton.setPosition(size.width / 2, size.height / 2 - 60);
-        mainUI.addChild(playWithAIButton);
+        this.addChild(playWithAIButton);
 
         var playDoubleButton = creator.createButton(txt.mainScene.playDouble, cc.size(280, 60), function () {
             cc.director.pushScene(new DoubleGameScene());
         });
         playDoubleButton.setPosition(size.width / 2, size.height / 2 - 150);
-        mainUI.addChild(playDoubleButton);
-
-        var logoutButton = creator.createButton(txt.mainScene.logout, cc.size(150, 60), function () {
-            player.logout();
-            loginUI.enabled = true;
-            loginUI.visible = true;
-            mainUI.enabled = false;
-            var moveAction = cc.moveTo(1, cc.p(0, 0)).easing(cc.easeExponentialInOut());
-            scrollableLayer.runAction(moveAction);
-        });
-        logoutButton.setPosition(size.width / 2, size.height / 2 - 260);
-        mainUI.addChild(logoutButton);
-
-        var playerLabel = creator.createLabel("", 25);
-        mainUI.addChild(playerLabel);
+        this.addChild(playDoubleButton);
+        
+        var playerLabel = new ccui.Button();
+        playerLabel.addClickEventListener(renameModalBox.popup.bind(renameModalBox));
+        playerLabel.titleColor = cc.color(0, 0, 0);
+        playerLabel.titleFontSize = 25;
+        this.addChild(playerLabel);
         function updatePlayerLabel() {
-            playerLabel.string = player.name;
+            playerLabel.titleText = player.name;
             playerLabel.setPosition(playerLabel.width / 2 + 30, size.height - playerLabel.height / 2 - 30);
         }
-
-        if (player.logged) {
-            loginUI.enabled = false;
-            loginUI.visible = false;
-            updatePlayerLabel();
-            scrollableLayer.x = -size.width;
-            playOnlineButton.enabled = !player.guest;
-        } else {
-            mainUI.enabled = false;
-        }
+        updatePlayerLabel();
 
         //固定界面
         var optionButton = new ccui.Button(res.OptionButtonN_png, res.OptionButtonS_png);
---
+++ client/src/ModalBox.js
@@ -0,0 +1,31 @@
+var ModalBox = cc.LayerColor.extend({
+    touchEvent: null,
+    ctor: function (width, height) {
+        this._super(cc.color.BLACK);
+        this.opacity = 80;
+        this.setVisible(false);
+        this.touchEvent = new cc.EventListener.create({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: false,
+            onTouchBegan: function (touch, event) {
+                return true;
+            }
+        });
+        cc.eventManager.addListener(this.touchEvent, this);
+        var background = new cc.Scale9Sprite(res.Button_png);
+        background.setPosition(size.width / 2, size.height / 2);
+        background.setContentSize(width,height);
+        this.addChild(background);
+        return true;
+    },
+
+    popup: function () {
+        this.setVisible(true);
+        this.touchEvent.setSwallowTouches(true);
+    },
+
+    hide: function () {
+        this.setVisible(false);
+        this.touchEvent.setSwallowTouches(false);
+    }
+});
